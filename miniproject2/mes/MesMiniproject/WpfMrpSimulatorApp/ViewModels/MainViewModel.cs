a439a3dc0473d9a4065fb3c84f481cbb41c6672e
phm0423 <qkrgusalsok@gmail.com>
2025-06-25 12:26:31 +0900

    8일

--- miniproject2/mes/MesMiniproject/WpfMrpSimulatorApp/ViewModels/MainViewModel.cs
+++ miniproject2/mes/MesMiniproject/WpfMrpSimulatorApp/ViewModels/MainViewModel.cs
@@ -73,5 +73,17 @@ namespace WpfMrpSimulatorApp.ViewModels
 
             CurrentView = view;
         }
+
+        [RelayCommand]
+        public void GetMonitoring()
+        {
+            var viewModel = new MonitoringViewModel(Common.DIALOGCOORDINATOR);
+            var view = new MonitoringView
+            {
+                DataContext = viewModel,
+            };
+
+            CurrentView = view;
+        }
     }
 }
