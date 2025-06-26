ccc3b5e7516e4b4df8d60fe83b01f537c7425b6c
phm0423 <qkrgusalsok@gmail.com>
2025-06-26 09:20:16 +0900

    Update App.xaml.cs

--- miniproject2/mes/MesMiniproject/WpfMqttSubApp/App.xaml.cs
+++ miniproject2/mes/MesMiniproject/WpfMqttSubApp/App.xaml.cs
@@ -18,7 +18,7 @@ namespace WpfMqttSubApp
         private void Application_Startup(object sender, StartupEventArgs e)
         {
             // config.json 파일 로드
-            Configuration = ConfigLoader.Load(); // config.json 파일 로드
+            Configuration = Helpers.ConfigLoader.Load(); // config.json 파일 로드
 
             // 뷰화면 로드 후 띄우기
             var coordinator = DialogCoordinator.Instance;  // new DialogCoordinator() 와 동일
